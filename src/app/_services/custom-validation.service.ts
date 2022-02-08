26b857f01bab370936b33ed7fa4e3672c7bd44ea
Thai Son Vu <vuthaison198x@gmail.com>
2022-02-08 10:47:49 +0700

    custom form validation

---
+++ src/app/_services/custom-validation.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { ValidatorFn, AbstractControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomValidationService {
+
+  constructor() { }
+
+  patternValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        return null as any;
+      }
+
+      /*
+        The password should be a minimum of eight characters long.
+        It has at least one lower case letter.
+        It has at least one upper case letter.
+        It has at least one number.
+      */
+      const regex = new RegExp('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+      const valid = regex.test(control.value);
+      return valid ? null as any : { invalidPassword: true };
+    };
+  }
+
+  MatchPassword(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[password];
+      const confirmPasswordControl = formGroup.controls[confirmPassword];
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return;
+      }
+
+      if (confirmPasswordControl.errors && !confirmPasswordControl.errors['passwordMismatch']) {
+        return;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    }
+  }
+}
