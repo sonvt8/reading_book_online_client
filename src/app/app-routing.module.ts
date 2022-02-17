c233514f1644e92c3be1c780ae36d80d8035ff4e
Liem <leliem28@gmail.com>
2022-02-17 14:18:44 +0700

    Add story component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,45 +15,48 @@ import { EditUserComponent } from './dashboard/user/edit-user/edit-user.componen
 import { ListStoryComponent } from './dashboard/story/list-story/list-story.component';
 import { ErrorComponent } from './error/error.component';
 import { ProfileComponent } from './account/profile/profile.component';
+import { StoryComponent } from './story/story.component';
 
 const routes: Routes = [
-  { path: 'trang-chu', component: HomeComponent, data: {title: 'Trang Chủ'}},
-  { 
-    path: 'thanh_vien', 
-    children:[
-      { path: '', component: MembersComponent, data: {title: 'Đăng nhập'}},
-      { path: 'dang_nhap', component: MembersComponent, data: {title: 'Đăng nhập'}},
-      { path: 'dang_ky', component: RegisterComponent, data: {title: 'Đăng ký'}},
-      { path: 'quen_mat_khau', component: ForgotPasswordComponent, data: {title: 'Quên mật khẩu'}}
+  { path: 'trang-chu', component: HomeComponent, data: { title: 'Trang Chủ' } },
+  {
+    path: 'thanh_vien',
+    children: [
+      { path: '', component: MembersComponent, data: { title: 'Đăng nhập' } },
+      { path: 'dang_nhap', component: MembersComponent, data: { title: 'Đăng nhập' } },
+      { path: 'dang_ky', component: RegisterComponent, data: { title: 'Đăng ký' } },
+      { path: 'quen_mat_khau', component: ForgotPasswordComponent, data: { title: 'Quên mật khẩu' } }
     ]
   },
-  { 
-    path: 'tai_khoan', 
-    children:[
-      { path: '', component: ProfileComponent, data: {title: 'Hồ sơ'}}
+  {
+    path: 'tai_khoan',
+    children: [
+      { path: '', component: ProfileComponent, data: { title: 'Hồ sơ' } }
     ]
   },
-  { 
-    path: 'quan-tri', 
+  {
+    path: 'quan-tri',
     component: AdminComponent,
-    children:[
-      { path: '', component: AdminHomeComponent, data: {title: 'Trang quản lý'}},
-      { path: 'trang-quan-ly', component: AdminHomeComponent, data: {title: 'Trang quản lý'}},
-      { path: 'the-loai', component: AdminCategoryComponent, data: {title: 'Danh sách thể loại'}},
-      { path: 'the-loai/:id', component: EditCategoryComponent, data: {title: ' Cập nhật Thể loại'}},
-      { path: 'them-the-loai', component: AddCategoryComponent, data: {title: 'Thêm thể loại'}},
-      { path: 'nguoi-dung', component: ListUserComponent, data: {title: 'Danh sách tài khoản'}},
-      { path: 'nguoi-dung/:id', component: EditUserComponent, data: {title: 'Cập nhật người dùng'} },
-      { path: 'truyen', component: ListStoryComponent, data: {title: 'Danh sách truyện'} },
+    children: [
+      { path: '', component: AdminHomeComponent, data: { title: 'Trang quản lý' } },
+      { path: 'trang-quan-ly', component: AdminHomeComponent, data: { title: 'Trang quản lý' } },
+      { path: 'the-loai', component: AdminCategoryComponent, data: { title: 'Danh sách thể loại' } },
+      { path: 'the-loai/:id', component: EditCategoryComponent, data: { title: ' Cập nhật Thể loại' } },
+      { path: 'them-the-loai', component: AddCategoryComponent, data: { title: 'Thêm thể loại' } },
+      { path: 'nguoi-dung', component: ListUserComponent, data: { title: 'Danh sách tài khoản' } },
+      { path: 'nguoi-dung/:id', component: EditUserComponent, data: { title: 'Cập nhật người dùng' } },
+      { path: 'truyen', component: ListStoryComponent, data: { title: 'Danh sách truyện' } },
     ]
   },
+  { path: 'truyen', component: StoryComponent, data: { title: 'Tên truyện' } },
+
   // Always put default route at the end.
-  { path: '', redirectTo: '/trang-chu', pathMatch: 'full', data: {title: 'Trang chủ'} },
+  { path: '', redirectTo: '/trang-chu', pathMatch: 'full', data: { title: 'Trang chủ' } },
   { path: '**', component: ErrorComponent },  // Wildcard route for a 404 page
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule] 
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Navigation, Autoplay, SwiperOptions } from 'swiper';
+
+SwiperCore.use([Navigation, Autoplay]);
 
 @Component({
   selector: 'app-story',
@@ -10,6 +13,11 @@ export class StoryComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  config: SwiperOptions = {
+    navigation: true,
+    autoplay: true
   }
 
 }
