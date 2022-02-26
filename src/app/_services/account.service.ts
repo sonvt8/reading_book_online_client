aa31e6e77f4cece04b9c284dbdef42d972d0b481
Thai Son Vu <vuthaison198x@gmail.com>
2022-02-26 15:33:50 +0700

    account delete story and fix bug status edited story

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -78,6 +78,10 @@ export class AccountService {
     return this.httpClient.post<Story>(`${this.baseUrl}tai-khoan/truyen/sua-truyen/${id}`, form);
   }
 
+  public deleteStory(id: number): Observable<CustomHttpResponse> {
+    return this.httpClient.delete<CustomHttpResponse>(`${this.baseUrl}tai-khoan/truyen/xoa-truyen/${id}`);
+  }
+
   public checkGetStoryById(id: number): Observable<CheckGetStoryResponseById> {
     return this.httpClient.get<CheckGetStoryResponseById>(`${this.baseUrl}tai-khoan/truyen/kiem-tra/${id}`);
   }
